f31bde11edde97da143af6007a9e4cf6f8b83199
lekithkishore <dasarilekithkishore@gmail.com>
2025-08-01 12:46:12 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,6 +13,9 @@ dotenv.config();
 app.use(cors());
 app.use(express.json()); // parses JSON
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML forms
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Test route
 app.get('/', (req, res) => {
